8bfbdca50de1bba8d8714e2c130208485ee24931
Minjoo-kang123 <rkdalswn0833@gmail.com>
2024-03-08 09:31:54 +0900

    second Commit

---
+++ day02/src/condition2.java
@@ -0,0 +1,12 @@
+public class condition2 {
+    public static void main(String[] args) {
+        int a = 1;
+        switch (a){
+            case 1:System.out.println("c");
+            case 2:System.out.println("R");
+            case 3:System.out.println("u");
+            case 4:System.out.println("d");
+            default:break;
+        }
+    }
+}
